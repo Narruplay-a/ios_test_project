961f26eeac515f6c6f82b112fc97a878a1d977b7
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2023-06-29 10:41:53 +0300

    Initial Commit

---
+++ ios_test_project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios_test_project
+//
+//  Created by Adel Khaziakhmetov on 29.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_test_project/ios_test_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_test_projectApp.swift
+//  ios_test_project
+//
+//  Created by Adel Khaziakhmetov on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ios_test_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
