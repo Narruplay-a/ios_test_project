3565010b437885aca8fa47451d67ce72eab71ebd
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2023-06-29 11:12:59 +0300

    first commit

---
+++ ios_test_projectTests/ios_test_projectTests.swift
@@ -0,0 +1,37 @@
+//
+//  ios_test_projectTests.swift
+//  ios_test_projectTests
+//
+//  Created by Adel Khaziakhmetov on 29.06.2023.
+//
+
+import XCTest
+
+/*
+ Написать несколько тестов:
+1. Тест проверки метода загрузки продуктов
+2. Тест производительности метода (память, процессор  время исполнения)
+*/
+
+final class ios_test_projectTests: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+        measure(
+                   
+        ) {
+            
+        }
+    }
+}
